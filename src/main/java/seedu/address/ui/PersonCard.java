afa2251870e49e78ab7f6555e45e0d94750dfd42
malcolmchu13 <malcolmchu@Malcolms-MacBook-Pro.local>
2025-10-16 18:37:08 +0800

    fix parent showing on gui for parent type contactts

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Student;
 
@@ -52,6 +53,10 @@ public class PersonCard extends UiPart<Region> {
     private Label cost;
     @FXML
     private CheckBox paidStatus;
+    @FXML
+    private VBox parentsContainer;
+    @FXML
+    private FlowPane parents;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -79,14 +84,27 @@ public class PersonCard extends UiPart<Region> {
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
 
+        parents.getChildren().clear();
+
         if (person instanceof Student student) {
             schedule.setText(student.getSchedule() != null
                     ? formatField("Schedule", student.getSchedule().value)
                     : formatField("Schedule", ""));
             schedule.setVisible(true);
+
+            parentsContainer.setManaged(true);
+            parentsContainer.setVisible(true);
+
+            student.getParents().stream()
+                    .sorted(Comparator.comparing(parent -> parent.getName().fullName))
+                    .forEach(parent -> parents.getChildren().add(new Label(parent.getName().fullName)));
         } else {
             schedule.setText("");
             schedule.setVisible(false);
+
+            parentsContainer.setManaged(false);
+            parentsContainer.setVisible(false);
+            parents.getChildren().clear();
         }
     }
 
