5fa99478e44396f7d1b1426ced143dd17902d3cc
evanjw2 <evanongjw@gmail.com>
2025-10-28 17:44:59 +0800

    Improve Schedule code quality

--- src/test/java/seedu/address/model/person/ScheduleTest.java
+++ src/test/java/seedu/address/model/person/ScheduleTest.java
@@ -62,4 +62,39 @@ public class ScheduleTest {
         Schedule schedule2 = new Schedule("Monday 14:00-16:00");
         assertEquals(schedule1.hashCode(), schedule2.hashCode());
     }
+
+    //Test to ensure output is standardized.
+    @Test
+    public void constructor_whitespaceAndCaseNormalization_success() {
+        Schedule schedule1 = new Schedule("monday     14:00-16:00");
+        assertEquals("Monday 14:00-16:00", schedule1.value);
+
+        Schedule schedule2 = new Schedule("MONDAY 14:00-16:00");
+        assertEquals("Monday 14:00-16:00", schedule2.value);
+
+        Schedule schedule3 = new Schedule("  12-10-2025   14:00-16:00  ");
+        assertEquals("12-10-2025 14:00-16:00", schedule3.value);
+    }
+
+    @Test
+    public void constructor_invalidTime_throwsIllegalArgumentException() {
+        // End time before start time
+        assertThrows(IllegalArgumentException.class, () -> new Schedule("Monday 16:00-14:00"));
+
+        //Invalid time format
+        assertThrows(IllegalArgumentException.class, () -> new Schedule("Monday 14-16"));
+        assertThrows(IllegalArgumentException.class, () -> new Schedule("Monday 14:00-1600"));
+    }
+
+    @Test
+    public void constructor_invalidDate_throwsIllegalArgumentException() {
+        // Invalid month/day
+        assertThrows(IllegalArgumentException.class, () -> new Schedule("13-32-2025 14:00-16:00"));
+    }
+
+    @Test
+    public void isEmpty_returnsTrueForEmptySchedule() {
+        Schedule emptySchedule = new Schedule("");
+        assertEquals(true, emptySchedule.isEmpty());
+    }
 }
